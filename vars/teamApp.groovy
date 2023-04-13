81f58e2174b2f17a632468f2aa5e745c1fee75a0
teeto815 <smartteeto@gmail.com>
2023-04-12 17:55:11 -0700

    new groovy file for second project

--- vars/teamApp.groovy
+++ vars/teamApp.groovy
@@ -1,1 +1,35 @@
+def call (String repoUrl){
+    pipeline{
+        agent any
+        tools{
+            maven 'maven'
+        }
+        stages{
+            stage("1-Tools initialization"){
+                steps{
+                    sh 'mvn --version'
+                    sh 'java --version'
+                }
 
+            }
+            stage("2-Checkout code"){
+                steps{
+                    git branch: 'main',
+                    url: "${repoUrl}"
+                }
+
+            }
+            stage("3-team5-test-shared"){
+                steps{
+                    echo "let us tesu this module"
+                }
+
+            }
+            stage("4-to test maven"){
+                steps{
+                    sh 'mvn -v'
+                }
+            }
+        }
+    }
+}
